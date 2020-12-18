6f58f1cd2221e0770290a4333fc18f4705203325
Johandry Amador <johandry@gmail.com>
2020-12-18 12:13:56 -0800

    draft for sync actions, not implemented yet

---
+++ schematics/actions_sync.go
@@ -0,0 +1,23 @@
+package schematics
+
+import "io"
+
+// CreateSync ...
+func (w *Workspace) CreateSync(writer io.Writer) (*Activity, error) {
+	return nil, nil
+}
+
+// PlanSync ...
+func (w *Workspace) PlanSync(writer io.Writer) (*Activity, error) {
+	return nil, nil
+}
+
+// ApplySync ...
+func (w *Workspace) ApplySync(writer io.Writer) (*Activity, error) {
+	return nil, nil
+}
+
+// DestroySync ...
+func (w *Workspace) DestroySync(writer io.Writer) (*Activity, error) {
+	return nil, nil
+}
